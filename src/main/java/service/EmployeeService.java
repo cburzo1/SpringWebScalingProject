bb576b8d9cb83b973609f9b533713e32b08088a1
cburzo1 <gm.c.burzo@gmail.com>
2025-04-18 08:25:15 -0400

    i

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> get();
+
+    Employee get(int id);
+
+    void save(Employee employee);
+
+    void delete(int id);
 }
--- src/main/java/service/EmployeeServiceImpl.java
+++ src/main/java/service/EmployeeServiceImpl.java
@@ -1,4 +1,40 @@
 package service;
 
+import dao.EmployeeDAO;
+import jakarta.transaction.Transactional;
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Transactional
+    @Override
+    public List<Employee> get() {
+        employeeDAO.get();
+    }
+
+    @Transactional
+    @Override
+    public Employee get(int id) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public void save(Employee employee) {
+
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+
+    }
 }
