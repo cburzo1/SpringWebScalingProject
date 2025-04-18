fb16eeeb91337746b90c1cd8049ea305a1670e82
cburzo1 <gm.c.burzo@gmail.com>
2025-04-18 07:27:16 -0400

    i

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -1,4 +1,15 @@
 package dao;
 
+import model.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+    List<Employee> get();
+
+    Employee get(int id);
+
+    void save(Employee employee);
+
+    void delete(int id);
 }
--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,4 +1,84 @@
 package model;
 
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity /*
+a real-world object or concept that you want to store information about.
+
+It typically corresponds to a table in a database.
+
+Each row in the table represents a specific instance of that entity.*/
+@Table(name = "tbl_Employee")
 public class Employee {
+    @id //Primary Key
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    /*This annotation tells JPA/Hibernate how to
+    automatically generate values for the primary key of an entity
+    (usually annotated with @Id). AUTO_INCREMENT*/
+    @Column
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String gender;
+    @Column
+    private String department;
+    @Column
+    private Date dob;
+
+    //Generate Setters and Getters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    //ToString:
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", department='" + department + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
 }
